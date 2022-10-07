b21b695f9c369c14a76bab62a4068c56d7c14d6a
Sevler <64084536+Furestry@users.noreply.github.com>
2022-10-07 14:59:35 +0300

    Created Project

---
+++ src/main/java/ru/furestry/sevlersite/SevlerSiteApplication.java
@@ -0,0 +1,13 @@
+package ru.furestry.sevlersite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevlerSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevlerSiteApplication.class, args);
+    }
+
+}
