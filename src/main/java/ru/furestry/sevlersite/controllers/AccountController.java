8c342218a25f2af03b04aa228057c80bb5659212
Sevler <64084536+Furestry@users.noreply.github.com>
2022-10-08 13:05:38 +0300

    Added template for upload images as user avatar

--- src/main/java/ru/furestry/sevlersite/controllers/AccountController.java
+++ src/main/java/ru/furestry/sevlersite/controllers/AccountController.java
@@ -1,16 +1,36 @@
 package ru.furestry.sevlersite.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.furestry.sevlersite.entities.db.User;
+import ru.furestry.sevlersite.repositories.UserRepository;
+
+import java.util.Base64;
 
 @Controller
 @RequestMapping("/account")
 public class AccountController {
 
+    private UserRepository userRepository;
+
     @GetMapping
-    public String getAccountPage() {
+    public String getAccountPage(Model model) {
+        User user = userRepository.findByUsername("");
+
+        model.addAttribute("user", user);
+
+        if (user.getAvatar() != null) {
+            model.addAttribute("userAvatar", Base64.getEncoder().encodeToString(user.getAvatar()));
+        }
+
         return "account";
     }
 
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
--- src/main/java/ru/furestry/sevlersite/entities/db/User.java
+++ src/main/java/ru/furestry/sevlersite/entities/db/User.java
@@ -17,4 +17,9 @@ public class User {
     private String username;
 
     private String password;
+
+    private String token;
+
+    @Lob
+    private byte[] avatar;
 }
