3b12a2097b4764ddeb2b254935c1255a9a86705f
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-02 11:43:55 +0300

    Removed unused import

--- src/main/java/ru/furestry/sevlersite/controllers/ApiController.java
+++ src/main/java/ru/furestry/sevlersite/controllers/ApiController.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
