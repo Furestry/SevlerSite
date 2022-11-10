8ee102847b98e7146c1a285f18dbfdfbac1a03c9
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-10 22:47:13 +0300

    Added method for find all comments by author id

--- src/main/java/ru/furestry/sevlersite/repositories/CommentRepository.java
+++ src/main/java/ru/furestry/sevlersite/repositories/CommentRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.furestry.sevlersite.entities.db.Comment;
 
+import java.util.Collection;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Collection<Comment> findByAuthorId(Long authorId);
+
 }
