fa2774cdce26a6099192850499d1805ccc76d997
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-10 18:14:26 +0300

    Created SetupDataLoader

---
+++ src/main/java/ru/furestry/sevlersite/configs/SetupDataLoader.java
@@ -0,0 +1,95 @@
+package ru.furestry.sevlersite.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import ru.furestry.sevlersite.entities.db.Privilege;
+import ru.furestry.sevlersite.entities.db.Role;
+import ru.furestry.sevlersite.entities.db.User;
+import ru.furestry.sevlersite.repositories.PrivilegeRepository;
+import ru.furestry.sevlersite.repositories.RoleRepository;
+import ru.furestry.sevlersite.repositories.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
+
+    private UserRepository userRepository;
+
+    private RoleRepository roleRepository;
+
+    private PrivilegeRepository privilegeRepository;
+
+    @Override
+    @Transactional
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (roleRepository.findAll().size() != 0) {
+            return;
+        }
+
+        Privilege readPrivilege = createPrivilegeIfNotFound("READ_PRIVILEGE");
+        Privilege writePrivilege = createPrivilegeIfNotFound("WRITE_PRIVILEGE");
+        Privilege deletePrivilege = createPrivilegeIfNotFound("DELETE_PRIVILEGE");
+
+        List<Privilege> adminPrivileges = Arrays.asList(readPrivilege, writePrivilege, deletePrivilege);
+
+        createRoleIfNotFound("ROLE_ADMIN", adminPrivileges);
+        createRoleIfNotFound("ROLE_USER", Arrays.asList(readPrivilege));
+
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN");
+
+        User user = new User();
+        user.setRoles(Arrays.asList(adminRole));
+
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public Privilege createPrivilegeIfNotFound(String name) {
+        Privilege privilege = privilegeRepository.findByName(name);
+
+        if (privilege == null) {
+            privilege = new Privilege();
+            privilege.setName(name);
+
+            privilegeRepository.save(privilege);
+        }
+
+        return privilege;
+    }
+
+    @Transactional
+    public Role createRoleIfNotFound(String name, Collection<Privilege> privileges) {
+        Role role = roleRepository.findByName(name);
+
+        if (role == null) {
+            role = new Role();
+            role.setName(name);
+            role.setPrivileges(privileges);
+
+            roleRepository.save(role);
+        }
+
+        return role;
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setPrivilegeRepository(PrivilegeRepository privilegeRepository) {
+        this.privilegeRepository = privilegeRepository;
+    }
+
+    @Autowired
+    public void setRoleRepository(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+}
