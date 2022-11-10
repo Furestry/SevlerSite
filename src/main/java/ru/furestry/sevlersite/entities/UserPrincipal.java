4ced12f6f567dfa603450270148cc6d825b280d0
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-10 17:29:43 +0300

    Added new entities and repositories (Privilege, Role and Comment)

--- src/main/java/ru/furestry/sevlersite/entities/UserPrincipal.java
+++ src/main/java/ru/furestry/sevlersite/entities/UserPrincipal.java
@@ -3,10 +3,11 @@ package ru.furestry.sevlersite.entities;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import ru.furestry.sevlersite.entities.db.Privilege;
+import ru.furestry.sevlersite.entities.db.Role;
 import ru.furestry.sevlersite.entities.db.User;
 
-import java.util.Collection;
-import java.util.Set;
+import java.util.*;
 
 public class UserPrincipal implements UserDetails {
 
@@ -18,7 +19,33 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Set.of(new SimpleGrantedAuthority("USER"));
+        return getGrantedAuthorities(getPrivileges((user.getRoles())));
+    }
+
+    private List<String> getPrivileges(Collection<Role> roles) {
+        List<String> privileges = new ArrayList<>();
+        List<Privilege> collection = new ArrayList<>();
+
+        for (Role role : roles) {
+            privileges.add(role.getName());
+            collection.addAll(role.getPrivileges());
+        }
+
+        for (Privilege item : collection) {
+            privileges.add(item.getName());
+        }
+
+        return privileges;
+    }
+
+    private Set<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (String privilege : privileges) {
+            authorities.add(new SimpleGrantedAuthority(privilege));
+        }
+
+        return authorities;
     }
 
     @Override
