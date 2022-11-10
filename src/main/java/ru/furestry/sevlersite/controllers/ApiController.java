ca232eaf908d86c8ee933581ca9a0da0d48946f5
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-10 22:46:54 +0300

    Added comments to api

--- src/main/java/ru/furestry/sevlersite/controllers/ApiController.java
+++ src/main/java/ru/furestry/sevlersite/controllers/ApiController.java
@@ -8,10 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import ru.furestry.sevlersite.entities.db.Comment;
 import ru.furestry.sevlersite.entities.db.User;
+import ru.furestry.sevlersite.repositories.CommentRepository;
 import ru.furestry.sevlersite.repositories.UserRepository;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Controller
@@ -28,6 +31,8 @@ public class ApiController {
     public class APIRestController {
 
         private UserRepository userRepository;
+
+        private CommentRepository commentRepository;
 
         @GetMapping(value = "/ping")
         public HttpStatus getPing() {
@@ -68,9 +73,52 @@ public class ApiController {
             return ResponseEntity.ok(new APIUser(user.getId(), user.getUsername()));
         }
 
+        @GetMapping(value = "/comments")
+        public List<Comment> getAllComments() {
+            return commentRepository.findAll();
+        }
+
+        @GetMapping(value = "/comments/me")
+        public ResponseEntity<Collection<Comment>> getMyComments(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+            User user = userRepository.findByTokenHash(token);
+
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
+            return ResponseEntity.ok(commentRepository.findByAuthorId(user.getId()));
+        }
+
+        @GetMapping(value = "/comments/{commentId}")
+        public ResponseEntity<Comment> getCommentById(@PathVariable Long commentId) {
+            Comment comment = commentRepository.findById(commentId).orElse(null);
+
+            if (comment == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok(comment);
+        }
+
+        @GetMapping(value = "/comments/user/{authorId}")
+        public ResponseEntity<Collection<Comment>> getCommentByUserId(@PathVariable Long authorId) {
+            Collection<Comment> comments = commentRepository.findByAuthorId(authorId);
+
+            if (comments == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok(comments);
+        }
+
         @Autowired
         public void setUserRepository(UserRepository userRepository) {
             this.userRepository = userRepository;
+        }
+
+        @Autowired
+        public void setCommentRepository(CommentRepository commentRepository) {
+            this.commentRepository = commentRepository;
         }
 
         @Data
