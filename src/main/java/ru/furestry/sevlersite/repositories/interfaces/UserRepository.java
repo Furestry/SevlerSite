b96a9fd7b9bd3b76a793d694ca992cf4f3066b62
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-14 02:12:40 +0300

    Moved

--- src/main/java/ru/furestry/sevlersite/repositories/interfaces/UserRepository.java
+++ src/main/java/ru/furestry/sevlersite/repositories/interfaces/UserRepository.java
@@ -1,4 +1,4 @@
-package ru.furestry.sevlersite.repositories;
+package ru.furestry.sevlersite.repositories.interfaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
