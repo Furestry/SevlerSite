8070bc33a8b6374644c1517e48e51863eefa8ef6
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-13 18:07:42 +0300

    Added users controller and template for comments functional

--- src/main/java/ru/furestry/sevlersite/controllers/UserController.java
+++ src/main/java/ru/furestry/sevlersite/controllers/UserController.java
@@ -5,15 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.furestry.sevlersite.entities.db.Role;
 import ru.furestry.sevlersite.entities.db.User;
 import ru.furestry.sevlersite.repositories.CommentRepository;
 import ru.furestry.sevlersite.repositories.UserRepository;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Principal;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -23,10 +26,30 @@ public class UserController {
     private CommentRepository commentRepository;
 
     @GetMapping("/id/{id}")
-    public String getUserById(@PathVariable Long id, Model model) {
+    public String getUserById(@PathVariable Long id, Model model, Principal principal) {
         User user = userRepository.findById(id).orElse(null);
 
         model.addAttribute("user", user);
+
+        if (user.getAvatar() != null) {
+            model.addAttribute("userAvatar", Base64.getEncoder().encodeToString(user.getAvatar()));
+        }
+
+        List<String> userRoles = user.getRoles().stream().map(Role::getName).toList();
+
+        if (userRoles.contains("ROLE_ADMIN")) {
+            model.addAttribute("isAdmin", true);
+        } else {
+            model.addAttribute("isAdmin", false);
+        }
+
+        if (principal != null) {
+            User principalUser = userRepository.findByUsername(principal.getName());
+
+            model.addAttribute("principal", principalUser);
+        }
+
+        model.addAttribute("comments", commentRepository.findAllByUserId(user.getId()));
 
         return "user";
     }
@@ -42,6 +65,22 @@ public class UserController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/create_comment")
+    public void createComment(@RequestParam Long user_id, @RequestBody String text, Principal principal) {
+        long createdAt = Instant.now().getEpochSecond();
+
+    }
+
+    @PostMapping("/edit_comment")
+    public void editComment(@RequestParam Long comment_id, @RequestBody String text, Principal principal) {
+
+    }
+
+    @DeleteMapping("/delete_comment")
+    public void deleteComment(@RequestParam Long comment_id, Principal principal) {
+
     }
 
     @Autowired
