aadbbc65f0edabc0a3af9a731002b7a0de739b86
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-17 19:29:06 +0300

    Renamed from UpdateService.java

--- src/main/java/ru/furestry/sevlersite/services/interfaces/INotificationService.java
+++ src/main/java/ru/furestry/sevlersite/services/interfaces/INotificationService.java
@@ -2,7 +2,7 @@ package ru.furestry.sevlersite.services.interfaces;
 
 import ru.furestry.sevlersite.entities.EventDto;
 
-public interface UpdateService {
+public interface INotificationService {
 
     void sendUpdate(long userId, EventDto event);
 
