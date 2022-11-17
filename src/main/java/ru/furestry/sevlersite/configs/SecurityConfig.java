dedfe7c40b8e5f27adda699308464c63db1fa9da
Sevler <64084536+Furestry@users.noreply.github.com>
2022-11-17 20:12:13 +0300

    Added public modification

--- src/main/java/ru/furestry/sevlersite/configs/SecurityConfig.java
+++ src/main/java/ru/furestry/sevlersite/configs/SecurityConfig.java
@@ -20,7 +20,7 @@ import ru.furestry.sevlersite.configs.handlers.CustomLogoutSuccessHandler;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
